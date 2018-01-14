9fa2b747990f5ed94101f8471fcb3e4b5263a6ad
rosew <rose@parksidesoftware.com>
2018-01-14 12:05:12 -0700

    Add error messages to user creation, new user fields and acceptance of terms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
 class User < ApplicationRecord
   include Clearance::User
 
+  attr_accessor :terms
+
   enum user_type: {
     basic_user: 1,
     admin_user: 2,
     pending_user: 3,
 
   }
+
+  validate :clicked_terms, :on => :create
+
+  private
+
+  def clicked_terms
+    errors.add(:terms, "Accept to continue.") if terms.blank? or terms == "0"
+  end
 
 end
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,5 @@
 Clearance.configure do |config|
+  config.routes = false
   config.mailer_sender = ENV['DEFAULT_EMAIL_FROM']
   config.rotate_csrf_on_sign_in = true
 end
