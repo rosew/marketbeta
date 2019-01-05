2269b53ae8bdbfd3051bae0971f45165f56d05b8
rosew <rose@parksidesoftware.com>
2019-01-05 12:51:49 -0700

    Fix Copyright

--- db/schema.rb
+++ db/schema.rb
@@ -48,4 +48,13 @@ ActiveRecord::Schema.define(version: 2018_11_27_203705) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  create_table "words", force: :cascade do |t|
+    t.string "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "mods"
+    t.text "raw_syllables"
+    t.text "raw_phonetics"
+  end
+
 end
